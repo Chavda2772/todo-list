c4711ca73169fb47f2df1b284cba10ef420999fb
Mahesh Chavda <66986475+Chavda2772@users.noreply.github.com>
2021-03-21 10:00:11 +0530

    localStorage implimented

--- main.js
+++ main.js
@@ -1,69 +1,86 @@
-const input = document.getElementById('input');
-const form = document.getElementById('form');
-var todoList = document.getElementById('todos');
-const storedTodo = JSON.parse(localStorage.getItem('todos'));
+const input = document.getElementById("input");
+const form = document.getElementById("form");
+var todoList = document.getElementById("todos");
+const storedTodo = JSON.parse(localStorage.getItem("todos"));
 
-if(storedTodo){
-    storedTodo.forEach(function(todo, idx){
-        addTodo(todo);
-    });
+if (storedTodo) {
+  storedTodo.forEach(function (todo, idx) {
+    addTodo(todo);
+  });
 }
 
-form.addEventListener('submit',function(e){
-    e.preventDefault();
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-    addTodo();
-    input.value = "";
+  addTodo();
+  input.value = "";
+  input.focus();
 });
 
-function addTodo(todo){
-    var todoText = input.value;
+function addTodo(todo) {
+  var todoText = input.value;
 
-    if(todo){
-        todoText = todo.text;
+  if (todo) {
+    todoText = todo.text;
+  }
+
+  if (todoText) {
+    const listEl = document.createElement("li");
+    const conDivEl = document.createElement("div");
+    conDivEl.classList.add("content");
+
+    const spanEl = document.createElement("span");
+    spanEl.innerText = todoText;
+
+    const AcDivEl = document.createElement("div");
+    AcDivEl.classList.add("action");
+
+    const deleteButton = document.createElement("button");
+    deleteButton.id = "delete";
+    deleteButton.addEventListener("click", (e) => {
+      listEl.remove();
+    });
+    deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
+
+    conDivEl.appendChild(spanEl);
+
+    AcDivEl.appendChild(deleteButton);
+
+    listEl.appendChild(conDivEl);
+    listEl.appendChild(AcDivEl);
+
+    if (todo) {
+      if (todo.isCompleted) {
+        listEl.classList.add("completed");
+      }
     }
 
-    if(todoText){
-        const spanEl = document.createElement('span');
-        spanEl.innerText = todoText;
-    
-        const divEl = document.createElement('div');
-        divEl.classList.add('content');
-        divEl.appendChild(spanEl);
-    
-        const listEl = document.createElement('li');
-        listEl.appendChild(divEl);
-
-        if(todo){
-            listEl.classList.add('completed');
-        }
-    
-        listEl.addEventListener('click',()=>{
-            listEl.classList.toggle('completed');
-            updateLocalStorage();
-        });
-    
-        listEl.addEventListener('contextmenu',(e)=>{
-            e.preventDefault();
-            listEl.remove();
-            updateLocalStorage();
-        });
-    
-        todoList.appendChild(listEl);
-        updateLocalStorage();
-    }
-}
-
-function updateLocalStorage(){
-    var localTodos = [];
-    
-    var todoLists = document.querySelectorAll('#todos li');
-    todoLists.forEach((todo,idx)=>{
-        localTodos.push({
-            text: todo.innerText,
-            isCompleted: todo.classList.contains('completed')
-        });
+    listEl.addEventListener("click", () => {
+      listEl.classList.toggle("completed");
+      updateLocalStorage();
     });
 
-    localStorage.setItem('todos',JSON.stringify(localTodos))
+    listEl.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+      listEl.remove();
+      updateLocalStorage();
+    });
+
+    todoList.appendChild(listEl);
+    updateLocalStorage();
+  }
+}
+
+function updateLocalStorage() {
+  var localTodos = [];
+
+  var todoLists = document.querySelectorAll("#todos li");
+  todoLists.forEach((todo, idx) => {
+    localTodos.push({
+      text: todo.innerText,
+      isCompleted: todo.classList.contains("completed"),
+    });
+  });
+
+  localStorage.setItem("todos", JSON.stringify(localTodos));
 }
